ee5c3095d3a50519b981cef3d7038d94f1f4b71f
Peter Tsai <peter.tsai@auckland.ac.nz>
2020-08-13 13:56:54 +1200

    Added vue-web-cam module

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,12 @@
+require('dotenv').config();
+
 export default {
 	
 	components: true,
+
+	buildModules: [
+		'@nuxtjs/dotenv',
+	],
 
 	modules: [
 		'@nuxtjs/onesignal',
@@ -15,7 +21,7 @@ export default {
 
 	oneSignal: {
 		init: {
-			appId: '0828e60c-c9b9-4ac0-a8c9-be89d67a9dd2',
+			appId: process.env.ONESIGNAL_APP_ID,
 			allowLocalhostAsSecureOrigin: true,
 			welcomeNotification: {
 				disable: true,
